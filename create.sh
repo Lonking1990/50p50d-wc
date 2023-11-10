e4191b4fccdbd1c4a8f1f2f220210c8b8420f0ed
Lonking <lonking1990@gmail.com>
2023-11-10 21:45:31 +0100

    Added script for generate folder from template and update index

---
+++ create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter the name of the new directory: " new_directory_name
+cp -r "0-template" "$new_directory_name"
+sed -i s/\<\!--template--\>/\<li\>\<a\ href\=\"\\/$new_directory_name\"\>$new_directory_name\<\\/a\>\<\\/li\>\\n\\t\\t\<\!--template--\>/ index.html
+echo "Directory created and index.html updated successfully."
